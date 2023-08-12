2db2539dc6ba9142532397440891df66000950d3
FRVR-JAE <tjoloyede9@gmail.com>
2023-08-12 05:23:32 -0700

    2

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express')
 const app = express()
 const path = require ('path')
 const PORT = process.env.PORT || 3500
-  
+const { logger } = require('./middleware/logger')
+app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
-app.all('*', (res, req) => {
+app.all('*', (req, res) => { 
     res.status(404)
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if (req.accepts('json')) {
-        res.json({ messages: '404 not found'})
+        res.json({ message: '404 not found'})
     } else {
         res.type('txt').send('404 not found')
     }
